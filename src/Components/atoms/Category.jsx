b7471efec497afe12b578b12d91d2d47cdde9a43
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-11-09 19:49:15 -0300

    Rquisito 29 e 30, ao clicar duas vezes no botao de categoria a lista de receitas reseta

--- src/Components/atoms/Category.jsx
+++ src/Components/atoms/Category.jsx
@@ -1,26 +1,42 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { getRecipesByCategory } from '../../redux/actions';
+import { getRecipesByCategory, getRecipes } from '../../redux/actions';
 
-const Category = ({ categoryName, foodOrDrink, fillRecipesByCategory }) => (
-  <button
-    type="button"
-    data-testid={ `${categoryName}-category-filter` }
-    onClick={ () => fillRecipesByCategory(categoryName, foodOrDrink) }
-  >
-    {categoryName}
-  </button>
-);
+const Category = ({
+  categoryName, foodOrDrink, fillRecipesByCategory, fillListOfRecipes,
+}) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <button
+      type="button"
+      data-testid={ `${categoryName}-category-filter` }
+      onClick={ () => {
+        if (isActive) {
+          setIsActive(false);
+          fillListOfRecipes(foodOrDrink);
+        } else {
+          setIsActive(true);
+          fillRecipesByCategory(categoryName, foodOrDrink);
+        }
+      } }
+    >
+      {categoryName}
+    </button>
+  );
+};
 
 Category.propTypes = {
   categoryName: PropTypes.string.isRequired,
+  fillListOfRecipes: PropTypes.func.isRequired,
   fillRecipesByCategory: PropTypes.func.isRequired,
   foodOrDrink: PropTypes.string.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
   fillRecipesByCategory: (c, f) => dispatch(getRecipesByCategory(c, f)),
+  fillListOfRecipes: (category) => dispatch(getRecipes(category)),
 });
 
 export default connect(null, mapDispatchToProps)(Category);
