c360833627a927cf3ed6585b9d526c42e1533b7f
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-11-24 14:44:57 -0300

    Clipboard test

--- src/Components/atoms/Category.jsx
+++ src/Components/atoms/Category.jsx
@@ -1,23 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { getRecipesByCategory, getRecipes } from '../../redux/actions';
+import { getRecipesByCategory } from '../../redux/actions';
 
 const Category = ({
-  categoryName, foodOrDrink, fillRecipesByCategory, fillListOfRecipes,
+  categoryName, foodOrDrink, fillRecipesByCategory,
 }) => (
   <button
     type="button"
     data-testid={ `${categoryName}-category-filter` }
     id={ categoryName }
     onClick={ () => {
-      if (isActive) {
-        setIsActive(false);
-        fillListOfRecipes(foodOrDrink);
-      } else {
-        setIsActive(true);
-        fillRecipesByCategory(categoryName, foodOrDrink);
-      }
+      fillRecipesByCategory(categoryName, foodOrDrink);
     } }
   >
     {categoryName}
@@ -25,14 +19,12 @@ const Category = ({
 );
 Category.propTypes = {
   categoryName: PropTypes.string.isRequired,
-  fillListOfRecipes: PropTypes.func.isRequired,
   fillRecipesByCategory: PropTypes.func.isRequired,
   foodOrDrink: PropTypes.string.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
   fillRecipesByCategory: (c, f) => dispatch(getRecipesByCategory(c, f)),
-  fillListOfRecipes: (category) => dispatch(getRecipes(category)),
 });
 
 export default connect(null, mapDispatchToProps)(Category);
