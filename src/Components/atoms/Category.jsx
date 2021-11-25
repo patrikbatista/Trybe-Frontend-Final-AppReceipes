83fc0a0b01880e7966ef00071806d43632bac876
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-11-25 10:47:54 -0300

    fixing tests

--- src/Components/atoms/Category.jsx
+++ src/Components/atoms/Category.jsx
@@ -1,30 +1,42 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { getRecipesByCategory } from '../../redux/actions';
+import { getRecipesByCategory, getRecipes } from '../../redux/actions';
 
 const Category = ({
-  categoryName, foodOrDrink, fillRecipesByCategory,
-}) => (
-  <button
-    type="button"
-    data-testid={ `${categoryName}-category-filter` }
-    id={ categoryName }
-    onClick={ () => {
+  categoryName, foodOrDrink, fillRecipesByCategory, fillRecipes,
+}) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+    if (isActive) {
+      fillRecipes(foodOrDrink);
+    } else {
       fillRecipesByCategory(categoryName, foodOrDrink);
-    } }
-  >
-    {categoryName}
-  </button>
-);
+    }
+  };
+  return (
+    <button
+      type="button"
+      data-testid={ `${categoryName}-category-filter` }
+      id={ categoryName }
+      onClick={ handleClick }
+    >
+      {categoryName}
+    </button>
+  );
+};
 Category.propTypes = {
   categoryName: PropTypes.string.isRequired,
+  fillRecipes: PropTypes.func.isRequired,
   fillRecipesByCategory: PropTypes.func.isRequired,
   foodOrDrink: PropTypes.string.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
   fillRecipesByCategory: (c, f) => dispatch(getRecipesByCategory(c, f)),
+  fillRecipes: (foodOrDrink) => dispatch(getRecipes(foodOrDrink)),
 });
 
 export default connect(null, mapDispatchToProps)(Category);
